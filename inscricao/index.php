b650d748576792b5810b033a9db3ecb29d5e3aea
fabiogiacomel <fabioceep@gmail.com>
2021-11-27 08:29:55 -0300

    Create index.php

---
+++ inscricao/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Inscrições On-Line 2022</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  
+<div class="container-fluid">
+  <h1>Inscrições encerradas</h1>
+  <p>Publicação da classificação no dia 06/12/2021</p>
+</div>
+
+</body>
+</html>
